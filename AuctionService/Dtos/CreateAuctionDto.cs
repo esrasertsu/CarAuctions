225f6ad4e14c7bc15fecd44bb6baeaa6cdb8dc36
esrasertsu <esrasertsu@gmail.com>
2024-11-16 13:42:41 +0300

    Init

---
+++ AuctionService/Dtos/CreateAuctionDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionService.Dtos
+{
+    public class CreateAuctionDto
+    {
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string Colour { get; set; }
+
+        [Required]
+        public int Mileaga { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public int ReservePrice { get; set; }
+
+        [Required]
+        public DateTime AuctionEnd { get; set; }
+    }
+}
