2f8f98cb2aa400a0740df3678f33067dacf9968f
esrasertsu <esrasertsu@gmail.com>
2024-11-25 01:46:10 +0300

    search service

---
+++ SearchService/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+using System.Text.Json;
+
+namespace SearchService.Data
+{
+    public class DbInitializer
+    {
+        public static async Task InitDb(WebApplication app)
+        {
+
+            await DB.InitAsync("SearchDb", MongoClientSettings
+                .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+            await DB.Index<Item>()
+                .Key(x => x.Make, KeyType.Text)
+                .Key(x => x.Model, KeyType.Text)
+                .Key(x => x.Colour, KeyType.Text)
+                .CreateAsync();
+
+            var count = await DB.CountAsync<Item>();
+
+            if (count == 0)
+            {
+                Console.WriteLine("No data - will attemp to send");
+                var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+                var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+                await DB.SaveAsync(items);
+            }
+        }
+    }
+}
--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,19 +1,29 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
 
 app.Run();
